de30166f3127086a1276a553a0f0e19c0083346b
princeXcode-ai <princekushwahap09@gmail.com>
2026-07-14 23:42:51 +0530

    TO-DO List added

---
+++ TO-DO_LIST.py
@@ -0,0 +1,70 @@
+print("===================")
+print("    TO-DO LIST")
+print("===================\n")
+
+tasks = []
+
+while True:
+    print("Press 1 for add task")
+    print("Press 2 for view tasks")
+    print("Press 3 for update task")
+    print("Press 4 for remove task\n")
+
+    check = int(input("Enter :- "))
+    
+    def add_task():
+        user = input("Add task :-").strip()
+        
+        if user== "":
+            print("No task added")
+            
+        else:
+            tasks.append(user)
+            print("\nTask successfully add")
+            
+        
+    def view_task():
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task}")
+        
+    def update_task():
+        print(view_task())
+        user_1 = int(input("Enter task number to update :- "))
+        new_task = input("Enter new task :- ")
+        tasks[user_1-1]= new_task
+        
+        print("\nTask updated successfully")
+        
+    def delete_file():
+        print(view_task())
+        user_2 = int(input("Enter task number to Remove :- "))
+        tasks.pop(user_2-1)
+        print("Task Removed successfully")
+          
+
+
+
+
+    if check == 1:
+        add_task()
+        
+    if check == 2:
+        view_task()
+    
+    if check == 3:
+        update_task()  
+        
+    if check == 4:
+        delete_file()
+        
+    user_l =input("do you want to do again [yes/no]:- ").lower() .strip()
+    
+    if user_l == "yes":
+        continue
+
+    elif user_l== "no":
+        print("THANKS for using TO-DO List")
+        break
+    else:
+        print("Invalid input! please enter only 'yes' or 'no'.")
+        
